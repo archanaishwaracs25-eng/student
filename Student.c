3ec7f787d688300e04733c9d1dd814d2ada6b2b8
archanapoojary <achhupujari@gmail.com>
2026-02-25 11:56:33 +0530

    started

---
+++ Student.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    struct Student {
+        int roll_no;
+        char name[200];
+        char mother_name[200];
+        char father_name[200];
+        int class;
+        char email[200];
+        char contact_num[10];
+
+    };
+}
